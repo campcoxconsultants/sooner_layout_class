4a6f80ef007c40963eafe19e9e14975566ac08b1
rebekahre <78057542+rebekahre@users.noreply.github.com>
2023-03-07 10:40:41 -0600

    updated icon_bar

--- lib/icon_bar.dart
+++ lib/icon_bar.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
 
+class IconBar extends StatelessWidget {
+  const IconBar({
+    super.key,
+    required this.onIconTap,
+  });
+
+  final void Function(String icon) onIconTap;
+
+  static const List<String> _icons = [
+    '🙂',
+    '😃',
+    '😍',
+    '😂',
+    '😭',
+    '😠',
+    '😡',
+    '😜',
+    '😘',
+    '😉',
+    '🤔',
+    '🤗',
+    '😷',
+    '🤢',
+    '🤮',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey[300]!,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          for (final icon in _icons)
+            GestureDetector(
+              onTap: () => onIconTap(icon),
+              child: Text(
+                icon,
+                style: const TextStyle(fontSize: 24),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
